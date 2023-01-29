e3f802d7fccbfc2a205fb17d612b60f4ddd494bc
Mary Frye <deimospho@gmail.com>
2023-01-28 20:20:38 -0500

    INITIAl changes, API fetch

--- public/index.js
+++ public/index.js
@@ -1,9 +1,31 @@
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
 async function main() {
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+    const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=fc2491deacd744d29efb73700ffaf2e6`)
+    const result = await response.json()
+    const { GME, MSFT, DIS, BNTX } = result;
+    const stocks = [GME, MSFT, DIS, BNTX];
+    stocks.forEach( stock => stock.values.reverse())
 
 }
 
 main()
+
+
+fc2491deacd744d29efb73700ffaf2e6
